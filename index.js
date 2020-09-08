29e258261df87345cb3ddfb2f7249cd6c9a83a6c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-08 02:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+  
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
@@ -12,6 +13,7 @@ const tutorials = [
 ];
 
 function titleCased() {
+<<<<<<< HEAD
 return tutorials.map(function (tutorial) {
   tutorial = tutorial.split(' ');
   for (var i = 0; i < tutorial.length; i++) {
@@ -19,4 +21,14 @@ return tutorials.map(function (tutorial) {
   }
   return tutorials
 }); 
+=======
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  });
+>>>>>>> 55f83c0423c1f3e862b98786091958641e7286d1
 }
+
